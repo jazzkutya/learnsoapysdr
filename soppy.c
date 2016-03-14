20df7602d2d3da406932172a69b0926193a81c25
jazzkutya <jazzkutya@gmail.com>
2016-03-14 14:12:21 +0100

    use c++ compiler, fixed some code and also commented them out :) segfault in soapysdr device enumeration

--- soppy.c
+++ soppy.c
@@ -8,12 +8,18 @@
 #include <SoapySDR/Device.h>
 
 int main(int argc, char *argv[]) {
-    size_t ndevs;
-    SoapySDRKwargs *devs=SoapySDRDevice_enumerate(NULL,&ndevs);
-    for (int t=0;t<ndevs;t++) {
+    size_t ndevs=0;
+    //SoapySDRKwargs *devs=SoapySDRDevice_enumerateStrArgs("",&ndevs);
+    SoapySDRKwargs filter;
+    filter.size=0;
+    SoapySDRKwargs *devs=SoapySDRDevice_enumerate(&filter,&ndevs);
+    // pfh... segfault in SoapySDRDevice_enumerateStrArgs and SoapySDRDevice_enumerate too
+    /*
+    printf("SoapySDR enumerated %zu devices\n",ndevs);
+    for (size_t t=0;t<ndevs;t++) {
         SoapySDRKwargs dev=devs[t];
-        printf("device %d (",t);
-        for (int k=0;k<dev.size;k++) {
+        printf("device %zu (",t);
+        for (size_t k=0;k<dev.size;k++) {
             char *key=dev.keys[t];
             char *val=dev.vals[t];
             printf("%s=%s",key,val);
@@ -21,4 +27,5 @@ int main(int argc, char *argv[]) {
         }
         printf(")\n");
     }
+    */
 }
